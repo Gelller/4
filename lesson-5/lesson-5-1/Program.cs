958f20f0bd1cc1a8486b405e6608c420677d890e
Gelller <geller.ggs@gmail.com>
2020-12-23 11:29:02 +0300

    Практическое задание 5

---
+++ lesson-5/lesson-5-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Наберите текст");
+
+             string filename="text.txt";
+             string str = Console.ReadLine();
+             File.WriteAllText(filename, str); 
+
+          
+            string fileText = File.ReadAllText(filename);
+             Console.WriteLine("Вывод текста текст");
+             Console.WriteLine(fileText); 
+             Console.ReadLine();
+        }
+    }
+}
---
+++ lesson-5/lesson-5-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //  Console.WriteLine("Наберите текст");
+
+             string filename="startup.txt";
+       //      string time = DateTime.Now;
+             File.WriteAllText(filename, DateTime.Now); 
+
+          
+            string fileText = File.ReadAllText(filename);
+             Console.WriteLine("Вывод текста текст");
+             Console.WriteLine(fileText); 
+             Console.ReadLine();
+        }
+    }
+}
