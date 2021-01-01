38efb4a8dd5273cfe99399554bd9d3a7ad4f2229
Gelller <geller.ggs@gmail.com>
2021-01-01 12:07:24 +0300

    Практическое задание 8

---
+++ lesson-8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.name))
+            {
+                Console.WriteLine("Введите имя");
+                Properties.Settings.Default.name = Console.ReadLine();
+                Properties.Settings.Default.Save();
+
+            }
+
+
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.age))
+            {
+
+                Console.WriteLine("Введите возраст");
+                Properties.Settings.Default.age = Console.ReadLine();
+                Properties.Settings.Default.Save();
+
+            }
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.work))
+            {
+                Console.WriteLine("Введите род деятельности");
+                Properties.Settings.Default.work = Console.ReadLine();
+                Properties.Settings.Default.Save();
+
+
+            }
+
+            Console.WriteLine($"Имя {Properties.Settings.Default.name}");
+            Console.WriteLine($"Возраст {Properties.Settings.Default.age}");
+            Console.WriteLine($"Род деятельности {Properties.Settings.Default.work}");
+
+            Console.ReadLine();
+        }
+    }
+}
